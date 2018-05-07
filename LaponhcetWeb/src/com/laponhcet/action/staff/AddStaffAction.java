31196cd811db1cbf26db5af897d6906b454a70ba
RichardChan <RichardChan@ISD-PF0ZH50A.local>
2018-05-07 14:14:37 +0800

    Staff

--- LaponhcetWeb/src/com/laponhcet/action/staff/AddStaffAction.java
+++ LaponhcetWeb/src/com/laponhcet/action/staff/AddStaffAction.java
@@ -13,14 +13,12 @@ public class AddStaffAction extends ActionBase {
 	private static final long serialVersionUID = 1L;
 	
 	protected void setSessionVars() {
-		sessionInfo.setTransitionLink(new String[] {"US0097", "US0079", "US0084" }, new String[] { "US0080", "US0085", "US0087" }, new String[] { "US0081", "US0086", "US0088" }, "US0082", "US0083");
+		sessionInfo.setTransitionLink(new String[] {"U00060", "U00055", "U00061" }, new String[] { "U00056", "U00062", "U00064" }, new String[] { "U00057", "U00063", "U00065" }, "U00058", "U00059");
 		if(!sessionInfo.isPreviousLinkAddSubmit()) {
 			setSessionAttribute(CityDTO.SESSION_CITY_LIST, new CityDAO().getCityList());
 			setSessionAttribute(ReligionDTO.SESSION_RELIGION_LIST, new ReligionDAO().getReligionList());
 			setSessionAttribute(AcademicProgramDTO.SESSION_ACADEMIC_PROGRAM_LIST, new AcademicProgramDAO().getAcademicProgramList());
 			setSessionBeforeTrans(StaffDTO.SESSION_STAFF, new StaffDTO());
 		}
-		
 	}
-
 }
